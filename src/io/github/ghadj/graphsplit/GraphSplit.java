e318b057ac1af5464a0a5d9616500c9e284dc79d
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2019-10-18 20:20:19 +0300

    comment changes

--- src/io/github/ghadj/graphsplit/GraphSplit.java
+++ src/io/github/ghadj/graphsplit/GraphSplit.java
@@ -15,6 +15,21 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
+ * There are the following options to be given as arguments to the program:
+ * 
+ * In case the first argument equals 0, the program promts user to give the path
+ * to the graph.txt file containing the following parameters, in the order shown
+ * below, each ending with newline: Number of nodes, Percentage of
+ * negative-value edges, Percentage of positive-value edges, Graph density,
+ * Adjacency Matrix, Adjacency Matrix with sign of each edge.
+ * 
+ * In case the first argument equals 1, the program generates a random graph
+ * based on the number of nodes, density of graph, precentage of positive and
+ * negative edges, that the user will be promted to enter
+ * 
+ * The program converts the problem into CNF (Conjunctive normal form) in order
+ * to be given in a SAT problem solver. Presents the solution from the SAT
+ * problem solver.
  * 
  * Compile: javac -d ./bin src/io/github/ghadj/graphsplit/*.java
  * 
